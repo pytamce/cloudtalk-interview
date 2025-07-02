2e291e52b02a034e84b351a2eb98d3a709a55a74
pytamce <80112290+pytamce@users.noreply.github.com>
2025-07-02 12:26:45 +0200

    Update apps.tf

--- dev/apps.tf
+++ dev/apps.tf
@@ -17,10 +17,35 @@ provider "aws" {
   }
 }
 
+data "aws_subnet" "app_private_subnet_1" {
+  filter {
+    name   = "tag:Name"
+    values = ["app-subnet-us-east-1a-private"]
+  }
+}
+
+data "aws_subnet" "app_private_subnet_2" {
+  filter {
+    name   = "tag:Name"
+    values = ["app-subnet-us-east-1b-private"]
+  }
+}
+
+locals {
+  apps_with_subnets = {
+    app1 = merge(var.applications["app1"], {
+      subnet_id = data.aws_subnet.app_private_subnet_1.id
+    })
+    app2 = merge(var.applications["app2"], {
+      subnet_id = data.aws_subnet.app_private_subnet_2.id
+    })
+  }
+}
+
 
 module "applications" {
   source            = "../modules/application"
-  for_each          = var.applications
+  for_each          = local.apps_with_subnets
   instance_type     = each.value.instance_type
   ami               = each.value.ami
   volume_size       = each.value.root_volume_size
